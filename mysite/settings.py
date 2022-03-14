157a5b5e636ead9419cefe00da51e839283d8d1e
Yarin Levi <74721108+yarinl3@users.noreply.github.com>
2022-03-14 19:02:58 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/home/yarinl3/yarinl3.pythonanywhere.com/static'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
