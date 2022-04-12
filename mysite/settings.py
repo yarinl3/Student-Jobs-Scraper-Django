d74bfb1980b771cd39e94242166d8c1579e1b8ef
Yarin Levi <74721108+yarinl3@users.noreply.github.com>
2022-04-12 23:44:57 +0300

    Add https protocol

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
