e5b747e3d0ec47231610322b915b256bcd7f7a5c
Yarin <yarinl330@gmail.com>
2022-04-12 11:47:02 +0300

    new structure for fast update jobs with unwanted keywords

--- main/Jobnet.py
+++ main/Jobnet.py
@@ -13,9 +13,7 @@ def jobnet(username):
     page = 0
     jobs = []
     while True:
-        url = f"https://www.jobnet.co.il/Jobs?p={page}&subprofid=1346,1283,752,766,770,1194,1506,763,1350,1073,1519," \
-              f"1526,1520,689,1511,696,729,733,764,892,904,1433,976,1394,979,1079,1119,1404&checkarea=2,5,9"
-        # url = https://www.jobnet.co.il/Jobs?subprofid=752,1073,1079&checkarea=2,5,9
+        url = f"https://www.jobnet.co.il/Jobs?p={page}&subprofid=752,1073,1079&checkarea=2,5,9"
         page += 1
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"}
         req = requests.get(url, headers=headers, verify=False).content.decode('utf-8')
